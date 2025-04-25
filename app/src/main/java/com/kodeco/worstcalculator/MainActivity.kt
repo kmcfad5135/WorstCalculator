01b330a81e16c827454b8d897573730361c57dcc
kmcfad5135 <kendall.mcfadden@howardcc.edu>
2025-04-24 23:23:25 -0400

    Worst Calculator

--- app/src/main/java/com/kodeco/worstcalculator/MainActivity.kt
+++ app/src/main/java/com/kodeco/worstcalculator/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        //Set the content
         setContent {
             WorstCalculatorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -37,34 +38,42 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WorstCalculator(modifier: Modifier = Modifier) {
+    //Create variables for the inputs
     var num1 by remember { mutableStateOf("") }
     var num2 by remember { mutableStateOf("") }
     var answer by remember { mutableStateOf<String?>(null) }
 
+    //Create a column to display fields vertically
     Column(
         modifier = modifier
             .padding(24.dp)
             .fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(20.dp)
     ) {
+        //Create a field for the first number
         OutlinedTextField(
             value = num1,
             onValueChange = { num1 = it },
             placeholder = { Text("Enter First Number") }
         )
+        //Create a field for the second number
         OutlinedTextField(
             value = num2,
             onValueChange = { num2 = it },
             placeholder = { Text("Enter Second Number") }
         )
+        //Create a button to initiate the calculation
         Button(onClick = {
             val n1 = num1.toInt()
             val n2 = num2.toInt()
+            //Solve the multiplication problem
             val sum = n1 * n2
             answer = sum.toString()
         }){
+            //Label the button
             Text("Calculate")
         }
+        //Display the answer if the conditions are correct
         answer?.let{
             Text("The answer is $it")
         }
@@ -75,6 +84,7 @@ fun WorstCalculator(modifier: Modifier = Modifier) {
 @Composable
 fun WorstCalculatorPreview() {
     WorstCalculatorTheme {
+        //Display the worst calculator
         WorstCalculator()
     }
 }
